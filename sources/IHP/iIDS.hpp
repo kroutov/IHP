dd9076ea8451f602865295463ad99ae0e0444551
jpclipffel <jp.clipffel@gmail.com>
2014-05-08 21:19:04 +0200

    Updated file headers to add license

--- sources/IHP/iIDS.hpp
+++ sources/IHP/iIDS.hpp
@@ -1,7 +1,20 @@
-/* 
-* @Author        : Jean-Philippe Clipffel
-* @Creation date : 2014-05-05 14:18:48
-* @Last update   : 2014-05-05 20:23:52
+/*
+* IHP - Intelligent Honeypot Project
+* Copyright (C) 2014 Jean-Philippe Clipffel
+* Email: jp.clipffel@gmail.com
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef IIDS_HPP
--- sources/IHP/modules/firewall/Firewall.hh
+++ sources/IHP/modules/firewall/Firewall.hh
@@ -1,3 +1,22 @@
+/*
+* IHP - Intelligent Honeypot Project
+* Copyright (C) 2014 Damien Meunier
+* Email: meunie_d@epitech.eu
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published
+* by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef				_FIREWALL_HH_
 # define			_FIREWALL_HH_
 
